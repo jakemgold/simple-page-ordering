eb0e48b293320d1603cd89fb3b4d8f094a6fc4cc
Felds Liscia <dev@felds.com.br>
2014-05-29 16:43:29 -0300

    translate description

--- simple-page-ordering.php
+++ simple-page-ordering.php
@@ -1,5 +1,5 @@
 <?php
-/**
+/*
 Plugin Name: Simple Page Ordering
 Plugin URI: http://10up.com/plugins/simple-page-ordering-wordpress/
 Description: Order your pages and hierarchical post types using drag and drop on the built in page list. For further instructions, open the "Help" tab on the Pages screen.
@@ -7,7 +7,9 @@ Version: 2.2
 Author: Jake Goldman, 10up
 Author URI: http://10up.com
 License: GPLv2 or later
- */
+Text Domain: simple-page-ordering
+Domain Path: /localization/
+*/
 
 if ( ! class_exists( 'Simple_Page_Ordering' ) ) :
 
@@ -294,3 +296,9 @@ class Simple_Page_Ordering {
 Simple_Page_Ordering::get_instance();
 
 endif;
+
+// dummy, to be used by poedit
+if (false) {
+	// Plugin description
+	__('Order your pages and hierarchical post types using drag and drop on the built in page list. For further instructions, open the "Help" tab on the Pages screen.', 'simple-page-ordering');
+}
